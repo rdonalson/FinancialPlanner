d5b0d2c32dc8cd833a349a3431d3d6fd39a150a0
Rick Donalson <rick.donalson@me.com>
2015-12-31 17:48:35 -0500

    Minor changes for new sql procedure "spCreateLedgerReadout"

--- FinancialPlanner.Data/Entity/enItemDetail.Designer.cs
+++ FinancialPlanner.Data/Entity/enItemDetail.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\rdonalson\Desktop\Programs\FinancialPlanner\FinancialPlanner.Data\Entity\enItemDetail.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\rdonalson\Source\Repos\FinancialPlanner\FinancialPlanner.Data\Entity\enItemDetail.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
---
+++ FinancialPlanner.Infrastructure/Domain/ItemDetail/Ledger/Repository/LedgerRepository.cs
@@ -0,0 +1,45 @@
+﻿using FinancialPlanner.Data.Entity;
+
+namespace FinancialPlanner.Infrastructure.Domain.ItemDetail.Ledger.Repository
+{
+    /// =====================================================================
+    /// <summary>
+    ///     Ledger Repository
+    /// </summary>
+    /// =====================================================================
+    public class LedgerRepository : ILedgerRepository
+    {
+        private readonly ItemDetailEntities _db;
+
+        /// ---------------------------------------------------------------------
+        /// <summary>
+        ///     Base Constructor
+        /// </summary>
+        /// ---------------------------------------------------------------------
+        public LedgerRepository(
+            int conndirection,
+            string server,
+            string database,
+            string userid,
+            string password)
+        {
+            _db = new ItemDetailEntities(ConnectionStringManager.ConnectionString(
+                conndirection,
+                2,
+                server,
+                database,
+                userid,
+                password));
+        }
+
+        /// ---------------------------------------------------------------------
+        /// <summary>
+        ///     Remove DBContext
+        /// </summary>
+        /// ---------------------------------------------------------------------
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+    }
+}
